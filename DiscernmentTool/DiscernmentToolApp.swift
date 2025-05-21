77f82e6f7ab85ae67ef89b3fda6057ee7eeef85e
Paul Lechner <paul.lechner@c12forums.com>
2025-05-20 22:07:15 -0400

    Initial Commit

---
+++ DiscernmentTool/DiscernmentToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscernmentToolApp.swift
+//  DiscernmentTool
+//
+//  Created by Paul Lechner on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscernmentToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
